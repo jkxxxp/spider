e06a13224c40f6c27629ef7b4023b9ba76155259
jkxxxp <46616536+jkxxxp@users.noreply.github.com>
2019-01-14 05:41:01 -0800

    Add files via upload

---
+++ text9.py
@@ -0,0 +1,35 @@
+
+import requests
+import re
+
+chapterList = []
+
+def getUrl():
+    url = "http://www.17k.com/list/2933095.html"
+    response = requests.get(url)
+    response.encoding = 'utf-8'
+    aimText = response.text.split('\n')
+    for addr in aimText:
+        try:
+            res = re.match(".*95/(.*?).html",addr)
+            res = '"http://www.17k.com" + "/chapter/2933095/" + res.group(1) + ".html"'
+            chapterList.append(res)
+        except Exception as e:
+            print(e)
+
+def getText():
+    for url in chapterList:
+        response = requests.get(url)
+        response.encoding = 'utf-8'
+        aimText_totle = response.text
+        regex = re.compile('&#12288;&#12288;(.*?)<br /><br />')
+        aimText = regex.findall(aimText_totle)
+        regex_title = re.compile("<title>(.*-.*? .*?)-")
+        aimText_title = regex_title.findall(aimText_totle)
+    with open('/home/xxp/Desktop/pp/{}.txt'.format(aimText_title[0]),mode='w',) as f:
+        for i in aimText:
+            f.write(i + '\r\n\n')
+
+if __name__ == "__main__":
+     getUrl()
+     getText()
